2e123141a7c2ae8fc5bbb42e5db52a6d028b74a7
Damián Parrilli <125889767+damianparrilli@users.noreply.github.com>
2025-08-07 20:22:05 -0300

    Big changes: Major changes: SEO-optimized language system, language injection adapted to this system. Using Astro as a SPA with Client Router (astro:transitions)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
-// https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
   server: {
     allowedHosts: [
-      'hunting-dealtime-insurance-bean.trycloudflare.com',
-      // Puedes agregar más hosts aquí si es necesario
+      "forbes-luther-length-ascii.trycloudflare.com"
     ]
   }
 });
